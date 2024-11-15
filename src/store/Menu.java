59c4ead18c1797b33466049834b768653ee850df
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2024-11-14 22:29:32 -0300

    Menu de Adm Adicionado
    Senha 123 para acessar

--- src/store/Menu.java
+++ src/store/Menu.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import store.controller.StoreController;
 import store.model.ProductDragon;
 import store.model.ProductJewellery;
+import store.util.Designs;
 
 public class Menu {
 
@@ -15,183 +16,241 @@ public class Menu {
 		StoreController products = new StoreController();
 		Scanner scan = new Scanner(System.in);
 
-		int option, prodNum, prodType, quantity;
+		int prodNum, prodType, quantity, password;
+		int option = 0, optionAdm = 0;
+		boolean admAccess = false;
 		String product, color, material;
 		float price;
 
-		while (true) {
-			System.out.println("Ms Zagia Draconic Grove");
-			System.out.println("Melhor bazar de Itens Draconicos!\n");
-			System.out.println("1- Criar novo item");
-			System.out.println("2- Listar todos os itens");
-			System.out.println("3- Editar item");
-			System.out.println("4- Deletar item");
-			System.out.println("5- Vender");
-			System.out.println("6- Comprar");
-			System.out.println("7- Buscar item por número");
-			System.out.println("8- Abastecer estoque");
-			System.out.println("0- Sair da Loja");
+		do {
 
-			try {
-				option = scan.nextInt();
-			} catch (InputMismatchException e) {
-				System.out.println("\nDigite um valor válido");
-				scan.nextLine();
-				option = 0;
-			}
+			if (admAccess) {
 
-			if (option == 0) {
-				System.out.println("Saindo da Loja...");
-				scan.close();
-				System.exit(0);
-			}
+				Designs.menuAdm();
 
-			switch (option) {
-			case 1:
-				System.out.println("Criar novo item");
-
-				System.out.println("Digite nome do Produto:  ");
-				scan.skip("\\R?");
-				product = scan.nextLine();
-				System.out.println("Digite o valor do Produto: ");
-				scan.skip("\\R?");
-				price = scan.nextFloat();
-				do {
-					System.out.println("Digite o tipo do Prduto: ");
-					System.out.println("1- Ovos de Dragão");
-					System.out.println("2- Jóias de Dragão");
-					scan.skip("\\R?");
-					prodType = scan.nextInt();
-				} while (prodType < 1 && prodType > 2);
-				System.out.println("Digite a quantidade em estoque: ");
-				quantity = scan.nextInt();
-				switch (prodType) {
-				case 1 -> {
-					System.out.println("Digite a cor do Dragão: ");
-					scan.skip("\\R?");
-					color = scan.nextLine();
-					products.create(
-							new ProductDragon(product, products.generateNum(), prodType, quantity, price, color));
-				}
-				case 2 -> {
-					System.out.println("Digite o material da Jóia: ");
-					scan.skip("\\R?");
-					material = scan.nextLine();
-					products.create(
-							new ProductJewellery(product, products.generateNum(), prodType, quantity, price, material));
+				try {
+					optionAdm = scan.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("\nDigite um valor válido");
+					scan.nextLine();
+					optionAdm = 0;
 				}
 
+				if (optionAdm == 0) {
+					System.out.println("Saindo da Loja...");
+					scan.close();
+					System.exit(0);
 				}
-				keyPress();
-				break;
-			case 2:
-				System.out.println("Listar todos os itens");
-				products.listAll();
-				keyPress();
-				break;
-			case 3:
-				System.out.println("Editar item");
-				System.out.println("Atualizar o cadastro do item\n");
-				System.out.println("Digite o número do produto: ");
-				prodNum = scan.nextInt();
-				var prodSearch = products.querryCollection(prodNum);
-				if (prodSearch != null) {
-					prodType = prodSearch.getProdType();
-					System.out.println("Digite o Nome do Produto: ");
+
+				switch (optionAdm) {
+
+				case 1:
+					System.out.println("Itens Disponíveis na Loja:");
+					products.listAll();
+					break;
+				case 2:
+					System.out.println("Criar novo item");
+
+					System.out.println("Digite nome do Produto:  ");
+					scan.skip("\\R?");
 					product = scan.nextLine();
+					System.out.println("Digite o valor do Produto: ");
+					scan.skip("\\R?");
+					price = scan.nextFloat();
+					do {
+						System.out.println("Digite o tipo do Prduto: ");
+						System.out.println("1- Ovos de Dragão");
+						System.out.println("2- Jóias de Dragão");
+						scan.skip("\\R?");
+						prodType = scan.nextInt();
+					} while (prodType < 1 && prodType > 2);
 					System.out.println("Digite a quantidade em estoque: ");
 					quantity = scan.nextInt();
-					System.out.println("Digite o preço do item: ");
-					price = scan.nextFloat();
 					switch (prodType) {
 					case 1 -> {
-						System.out.println("Digite a cor do Ovo: ");
-						color = scan.next();
-						products.update(
+						System.out.println("Digite a cor do Dragão: ");
+						scan.skip("\\R?");
+						color = scan.nextLine();
+						products.create(
 								new ProductDragon(product, products.generateNum(), prodType, quantity, price, color));
 					}
 					case 2 -> {
-						System.out.println("Digite o material da jóia dracônica");
-						material = scan.next();
-						products.update(new ProductDragon(product, products.generateNum(), prodType, quantity, price,
+						System.out.println("Digite o material da Jóia: ");
+						scan.skip("\\R?");
+						material = scan.nextLine();
+						products.create(new ProductJewellery(product, products.generateNum(), prodType, quantity, price,
 								material));
 					}
-					default -> {
-						System.out.println("Tipo de produto inválido!");
+
 					}
+					keyPress();
+					break;
+				case 3:
+					System.out.println("Editar item");
+					System.out.println("Atualizar o cadastro do item\n");
+					System.out.println("Digite o número do produto: ");
+					prodNum = scan.nextInt();
+					var prodSearch = products.querryCollection(prodNum);
+					if (prodSearch != null) {
+						prodType = prodSearch.getProdType();
+						System.out.println("Digite o Nome do Produto: ");
+						product = scan.nextLine();
+						System.out.println("Digite a quantidade em estoque: ");
+						quantity = scan.nextInt();
+						System.out.println("Digite o preço do item: ");
+						price = scan.nextFloat();
+						switch (prodType) {
+						case 1 -> {
+							System.out.println("Digite a cor do Ovo: ");
+							color = scan.next();
+							products.update(new ProductDragon(product, products.generateNum(), prodType, quantity,
+									price, color));
+						}
+						case 2 -> {
+							System.out.println("Digite o material da jóia dracônica");
+							material = scan.next();
+							products.update(new ProductDragon(product, products.generateNum(), prodType, quantity,
+									price, material));
+						}
+						default -> {
+							System.out.println("Tipo de produto inválido!");
+						}
+						}
+					} else {
+						System.out.println("O produto não foi encontrado!");
 					}
-				} else {
-					System.out.println("O produto não foi encontrado!");
+
+					keyPress();
+					break;
+				case 4:
+					System.out.println("Deletar item");
+					System.out.println("Digite o número do produto: ");
+					prodNum = scan.nextInt();
+					products.delete(prodNum);
+					keyPress();
+					break;
+
+				case 5:
+					System.out.println("Abastecer estoque");
+
+					// Teste do Produto Ovo de Dragão
+					ProductDragon babyDragon1 = new ProductDragon("Ovo de Dragão", products.generateNum(), 1, 50,
+							500.0f, "Verde Musgo");
+					products.create(babyDragon1);
+					babyDragon1.visualize();
+					System.out.println(" ");
+
+					ProductDragon babyDragon2 = new ProductDragon("Ovo de Dragão", products.generateNum(), 1, 20,
+							1200.0f, "Preto Azulado");
+					products.create(babyDragon2);
+					babyDragon2.visualize();
+					System.out.println(" ");
+
+					// Teste do Produto Jóias de Dragão
+					ProductJewellery dragonGem1 = new ProductJewellery("Colar de Esmeraldas", products.generateNum(), 2,
+							8, 1200.0f, "Platina e Esmeraldas Lapidadas");
+					products.create(dragonGem1);
+					dragonGem1.visualize();
+					System.out.println(" ");
+
+					ProductJewellery dragonGem2 = new ProductJewellery("Colar de Diamantes", products.generateNum(), 2,
+							1, 1200.0f, "Ouro e Diamantes");
+					products.create(dragonGem2);
+					dragonGem2.visualize();
+					System.out.println(" ");
+
+					keyPress();
+					break;
+
+				case 6:
+					admAccess = false;
+					break;
+
+				default:
+					System.out.println("Opção inválida");
+					break;
+
 				}
 
-				keyPress();
-				break;
-			case 4:
-				System.out.println("Deletar item");
-				System.out.println("Digite o número do produto: ");
-				prodNum = scan.nextInt();
-				products.delete(prodNum);
-				keyPress();
-				break;
-			case 5:
-				System.out.println("Vender item");
-				System.out.println("Digite o número do produto: ");
-				prodNum = scan.nextInt();
-				do {
-					System.out.println("Digite a quantidade que deseja vender ");
-					quantity = scan.nextInt();
-				} while (quantity <= 0);
-				products.sell(prodNum, quantity);
-				keyPress();
-				break;
-			case 6:
-				System.out.println("Comprar item");
-				System.out.println("Digite o número do produto: ");
-				prodNum = scan.nextInt();
-				do {
-					System.out.println("Digite a quantidade desejada: ");
-					quantity = scan.nextInt();
-				} while (quantity <= 0);
-				products.buy(prodNum, quantity);
+			} else {
 
-				keyPress();
-				break;
-			case 7:
-				System.out.println("Verificar item por número");
+				Designs.menu();
 
-				System.out.println("Digite o número do produto: ");
-				prodNum = scan.nextInt();
-				products.searchByProdNum(prodNum);
+				try {
+					option = scan.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("\nDigite um valor válido");
+					scan.nextLine();
+					option = 0;
+				}
 
-				keyPress();
-				break;
-			case 8:
-				System.out.println("Abastecer estoque");
-				// Teste do Produto Ovo de Dragão
-				ProductDragon babyDragon1 = new ProductDragon("Ovo de Dragão", 123, 1, 50, 500.0f, "Verde Musgo");
-				babyDragon1.visualize();
-				
-				ProductDragon babyDragon2 = new ProductDragon("Ovo de Dragão", 789, 1, 20, 1200.0f, "Preto Azulado");
-				babyDragon2.visualize();
+				if (option == 0) {
+					System.out.println("Saindo da Loja...");
+					scan.close();
+					System.exit(0);
+				}
 
-				// Teste do Produto Jóias de Dragão
-				ProductJewellery dragonGem1 = new ProductJewellery("Colar de Esmeraldas", 456, 2, 8, 1200.0f,
-						"Platina e Esmeraldas Lapidadas");
-				dragonGem1.visualize();
-				
-				ProductJewellery dragonGem2 = new ProductJewellery("Colar de Diamantes", 890, 2, 1, 1200.0f,
-						"Ouro e Diamantes");
-				dragonGem2.visualize();
+				switch (option) {
 
-				keyPress();
-				break;
+				case 1:
+					System.out.println("Insira a senha:");
+					scan.skip("\\R?");
+					password = scan.nextInt();
+					if (password != 123) {
+						System.out.println("Senha incorreta, digite a senha 123 para acessar o Menu de Administração");
+						admAccess = false;
+					} else {
+						admAccess = true;
+					}
+					keyPress();
+					break;
 
-			default:
-				System.out.println("Opção inválida");
-				break;
+				case 2:
+					System.out.println("Itens disponíveis na loja: ");
+					products.listAll();
+					keyPress();
+					break;
+				case 3:
+					System.out.println("Vender item");
+					System.out.println("Digite o número do produto: ");
+					prodNum = scan.nextInt();
+					do {
+						System.out.println("Digite a quantidade que deseja vender ");
+						quantity = scan.nextInt();
+					} while (quantity <= 0);
+					products.sell(prodNum, quantity);
+					keyPress();
+					break;
+				case 4:
+					System.out.println("Comprar item");
+					System.out.println("Digite o número do produto: ");
+					prodNum = scan.nextInt();
+					do {
+						System.out.println("Digite a quantidade desejada: ");
+						quantity = scan.nextInt();
+					} while (quantity <= 0);
+					products.buy(prodNum, quantity);
+
+					keyPress();
+					break;
+				case 5:
+					System.out.println("Verificar item por número");
+
+					System.out.println("Digite o número do produto: ");
+					prodNum = scan.nextInt();
+					products.searchByProdNum(prodNum);
+
+					keyPress();
+					break;
+
+				default:
+					System.out.println("Opção inválida");
+					break;
+				}
+
 			}
 
-		}
+		} while(option != 0 || optionAdm != 0);
 
 	}
 
@@ -208,5 +267,4 @@ public class Menu {
 
 		}
 	}
-
 }
---
+++ src/store/util/Designs.java
@@ -0,0 +1,63 @@
+package store.util;
+
+public class Designs {
+	
+	public static void menu() {
+		System.out.println("                                                                    ");
+		System.out.println(" ╔════════════════════════════════════════════════════════════════╗ ");
+		System.out.println(""
+				+ " ║                         ┳┳┓   ┏┓    •                          ║\r\n"
+				+ " ║                         ┃┃┃┏  ┏┛┏┓┏┓┓┏┓                        ║\r\n"
+				+ " ║                         ┛ ┗┛  ┗┛┗┻┗┫┗┗┻                        ║\r\n"
+				+ " ║                                    ┛                           ║\r\n"
+				+ " ║                     ┳┓         •   ┏┓                          ║\r\n"
+				+ " ║                     ┃┃┏┓┏┓┏┏┓┏┓┓┏  ┃┓┏┓┏┓┓┏┏┓                  ║\r\n"
+				+ " ║                     ┻┛┛ ┗┻┗┗┛┛┗┗┗  ┗┛┛ ┗┛┗┛┗                   ║\r\n"
+				+ " ║                                                                ║");
+				
+		System.out.println(" ╠════════════════════════════════════════════════════════════════╣ ");
+		System.out.println(" ║                                                                ║ ");
+		System.out.println(" ║                  1 - Entrar no Menu de ADM                     ║ ");
+		System.out.println(" ║                  2 - Itens Disponíveis                         ║ ");
+		System.out.println(" ║                  3 - Vender item                               ║ ");
+		System.out.println(" ║                  4 - Comprar item                              ║ ");
+		System.out.println(" ║                  5 - Buscar item por número                    ║ ");
+		System.out.println(" ║                  0 - Sair da Loja                              ║ ");
+		System.out.println(" ║                                                                ║ ");
+		System.out.println(" ╚════════════════════════════════════════════════════════════════╝ ");
+		System.out.println("                                                                    ");
+	}
+	
+	public static void menuAdm() {
+		System.out.println("╭―――――――――――――――――――――――――――――――――――――――――――╮");
+		System.out.println("│          ⚝ Menu de Administração ⚝        │");
+		System.out.println("╰―――――――――――――――――――――――――――――――――――――――――――╯");
+		System.out.println("╭―――――――――――――――――――――――――――――――――――――――――――╮");
+		System.out.println("│                                           │");
+		System.out.println("│           1 - Itens Disponíveis           │");
+		System.out.println("│           2 - Criar novo item             │");
+		System.out.println("│           3 - Editar item                 │");
+		System.out.println("│           4 - Deletar item                │");
+		System.out.println("│           5 - Abastecer estoque           │");
+		System.out.println("│           6 - Voltar para a Loja          │");
+		System.out.println("│           0 - Sair da Loja                │");
+		System.out.println("│                                           │");
+		System.out.println("╰―――――――――――――――――――――――――――――――――――――――――――╯");
+		
+	}
+	
+	/*
+	System.out.println("Ms Zagia Draconic Grove");
+	System.out.println("Melhor bazar de Itens Draconicos!\n");
+	System.out.println("1- Criar novo item");
+	System.out.println("2- Listar todos os itens");
+	System.out.println("3- Editar item");
+	System.out.println("4- Deletar item");
+	System.out.println("5- Vender");
+	System.out.println("6- Comprar");
+	System.out.println("7- Buscar item por número");
+	System.out.println("8- Abastecer estoque");
+	System.out.println("0- Sair da Loja");
+	*/
+
+}
